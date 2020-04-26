de31744fd988854558fe70c8438b8ec6d23bfa2a
lainz <leandrodiazoe@gmail.com>
2020-04-26 01:00:38 -0300

    Cloning controls.

---
+++ uclonecontrols.pp
@@ -0,0 +1,80 @@
+unit uCloneControls;
+
+{
+  Author: Lainz and howardpc
+  Original From Lainz: https://forum.lazarus.freepascal.org/index.php/topic,24239.msg287928.html#msg287928
+  Latest Source From howardpc: https://forum.lazarus.freepascal.org/index.php/topic,24239.msg288051.html#msg288051
+}
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, TypInfo, Controls;
+
+procedure CloneControls(const aControl: TControl; const aNewOwnerParent: TWinControl; anIndex: Integer);
+
+procedure CloneEvents(aSourceControl, aClonedControl: TControl);
+
+procedure CloneProperties(aSourceControl: TControl; aClonedControl: TControl);
+
+
+implementation
+
+procedure CloneControls(const aControl: TControl; const aNewOwnerParent: TWinControl; anIndex: Integer);
+var
+  i: Integer;
+  currentControl, clonedControl: TControl;
+  winControl: TWinControl absolute currentControl;
+  s: String;
+begin
+  currentControl := aControl;
+  clonedControl := TControlClass(currentControl.ClassType).Create(aNewOwnerParent);
+  clonedControl.Parent := aNewOwnerParent;
+  CloneProperties(currentControl, clonedControl);
+  WriteStr(s, anIndex);
+  clonedControl.Name := 'CloneOf_' + currentControl.Name + s;
+  // Debug
+  clonedControl.Hint := clonedControl.Name;
+  CloneEvents(currentControl, clonedControl);
+  if clonedControl is TWinControl then
+    // Lainz: changed order to preserve positioning
+    for i := 0 to winControl.ControlCount - 1 do
+      CloneControls(winControl.Controls[i], TWinControl(clonedControl), i);
+end;
+
+procedure CloneEvents(aSourceControl, aClonedControl: TControl);
+var
+  i: integer;
+  propList: TPropList;
+begin
+  Assert(aSourceControl.ClassNameIs(aClonedControl.ClassName),'CloneEvents: incompatible controls');
+  for i := 0 to GetPropList(aSourceControl.ClassInfo, [tkMethod], @propList) - 1 do
+    SetMethodProp(aClonedControl, propList[i], GetMethodProp(aSourceControl, propList[i]));
+end;
+
+procedure CloneProperties(aSourceControl: TControl; aClonedControl: TControl);
+var
+  ms: TMemoryStream;
+  oldName: String;
+begin
+  Assert(aSourceControl.ClassNameIs(aClonedControl.ClassName),'CloneProperties: incompatible controls');
+  oldName := aSourceControl.Name;
+  aSourceControl.Name := '';
+  try
+    ms := TMemoryStream.Create;
+    try
+      ms.WriteComponent(aSourceControl);
+      ms.Position := 0;
+      ms.ReadComponent(aClonedControl);
+    finally
+      ms.Free;
+    end;
+  finally
+    aSourceControl.Name := oldName;
+  end;
+end;
+
+end.
+
--- umain.pas
+++ umain.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
-  BCPanel, BCLabel, BCComboBox, fphttpclient, fpjson;
+  BCPanel, BCLabel, BCComboBox, fphttpclient, fpjson, uCloneControls, LCLIntF;
 
 resourcestring
   rsCantLoadJSON = 'Can''''t load JSON from server.';
@@ -38,11 +38,16 @@ type
     edtSearch: TEdit;
     bcpPackageLPK: TPanel;
     bcpLinks: TPanel;
+    sbMain: TScrollBox;
+    procedure bclLinkMouseEnter(Sender: TObject);
+    procedure bclLinkMouseLeave(Sender: TObject);
+    procedure bclLinkClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
   private
-    JSON: TJSONArray;
+    JSON: TJSONObject;
     procedure LoadJSON;
+    procedure DisplayJSON;
   public
 
   end;
@@ -56,9 +61,25 @@ implementation
 
 { TfrmMain }
 
+procedure TfrmMain.bclLinkMouseEnter(Sender: TObject);
+begin
+  TBCLabel(Sender).FontEx.Style := [fsUnderline];
+end;
+
+procedure TfrmMain.bclLinkMouseLeave(Sender: TObject);
+begin
+  TBCLabel(Sender).FontEx.Style := [];
+end;
+
+procedure TfrmMain.bclLinkClick(Sender: TObject);
+begin
+  OpenURL(TControl(Sender).Hint);
+end;
+
 procedure TfrmMain.FormCreate(Sender: TObject);
 begin
   LoadJSON;
+  DisplayJSON;
 end;
 
 procedure TfrmMain.FormDestroy(Sender: TObject);
@@ -73,13 +94,47 @@ var
 begin
   try
     data := TFPHTTPClient.SimpleGet('http://packages.lazarus-ide.org/packagelist.json');
-    JSON := TJSONArray(GetJSON(data));
+    JSON := TJSONObject(GetJSON(data));
   except
     on e: Exception do
     begin
       ShowMessage(rsCantLoadJSON + LineEnding + e.Message);
       Application.Terminate;
     end;
+  end;
+end;
+
+procedure TfrmMain.DisplayJSON;
+var
+  i: integer;
+  data: TJSONObject;
+  files: TJSONArray;
+  ctrl, bcp: TControl;
+  bcl: TBCLabel;
+begin
+  i := 0;
+  while (JSON.FindPath('PackageData' + IntToStr(i)) <> nil) do
+  begin
+    data := TJSONObject(JSON.FindPath('PackageData' + IntToStr(i)));
+    files := TJSONArray(JSON.FindPath('PackageFiles' + IntToStr(i)));
+    CloneControls(bcpPackage, sbMain, i);
+    ctrl := sbMain.Controls[i];
+    if Assigned(ctrl) then
+    begin
+
+      bcl := TBCLabel(ctrl.FindComponent('CloneOf_bclPackageTitle0'));
+      bcl.Caption := data.FindPath('DisplayName').AsString;
+      bcp := TControl(ctrl.FindComponent('CloneOf_bcpLinks2'));
+      bcp.Caption := '';
+      bcp := TControl(ctrl.FindComponent('CloneOf_bcpPackageLPK3'));
+      bcp.Caption := '';
+      ctrl.Caption := '';
+      ctrl.Visible := True;
+    end;
+    Inc(i);
+    // ToDo: limit of scrollbox height
+    if (i >= 10) then
+      break;
   end;
 end;
 
